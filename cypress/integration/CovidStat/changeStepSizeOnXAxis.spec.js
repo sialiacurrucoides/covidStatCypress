66f5d04efe9913224efba6e8d6e8b83e0734f3d6
Simon Júlia <simon.julia@nexogen.hu>
2021-07-07 07:48:58 +0200

    test: slider endpoint dates and update name change

--- cypress/integration/CovidStat/changeStepSizeOnXAxis.spec.js
+++ cypress/integration/CovidStat/changeStepSizeOnXAxis.spec.js
@@ -11,7 +11,7 @@ describe("change step size on the x axis via select", () => {
     });
 
     it("should change the 14 days resolution to 15", () => {
-        const stepSize = cy.get("span").contains("Step size (x-axis): ").next().find("input");
+        const stepSize = cy.get("span").contains("Data range (x-axis): ").next().find("input");
         stepSize.should("have.value","14");
         stepSize.type("{backspace}{backspace}17");
         stepSize.should("have.value","17");
--- cypress/integration/CovidStat/setXAxisBySlider.spec.js
+++ cypress/integration/CovidStat/setXAxisBySlider.spec.js
@@ -5,7 +5,7 @@ describe("change step size on the x axis via select", () => {
         cy.waitForData(data);
         cy.get("button").contains("infected").click();
         cy.get("button").contains("deceased").click();
-        cy.get("span").contains("Step size (x-axis): ").next().find("input")
+        cy.get("span").contains("Data range (x-axis): ").next().find("input")
         .type("{backspace}{backspace}5");
     });
 
@@ -13,6 +13,11 @@ describe("change step size on the x axis via select", () => {
         cy.get(".xAxis").find("tspan").first().contains("Jun/30");
         cy.get(".MuiSlider-mark[data-index='0']").click();
         cy.get(".xAxis").find("tspan").first().contains("Jun/11");
+    });
+
+    it("should show the first and last date below the endpoints", () => {
+        cy.get("span[aria-label='first date']").contains("2021/Jun");
+        cy.get("span[aria-label='last date']").contains("2021/Jul");;
     });
 
 });
